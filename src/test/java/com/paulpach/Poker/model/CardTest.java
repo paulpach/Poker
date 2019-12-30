279c0194d14195b53b1aa8fd9e58909ab34fa5e6
Paul Pacheco <paulpach@gmail.com>
2019-12-30 09:13:33 -0600

    test constructor

--- src/test/java/com/paulpach/Poker/model/CardTest.java
+++ src/test/java/com/paulpach/Poker/model/CardTest.java
@@ -1,12 +1,15 @@
 package com.paulpach.Poker.model;
 
+import com.paulpach.Poker.model.enums.CardType;
+
 import org.junit.jupiter.api.Test;
 
 class CardTest {
 
 	@Test
 	public void TestCard() {
-		Card card;
+        Card card = new Card(10, CardType.CLUBS);
+        
 	}
 	
 }
