e44dddd49a8f65fae7043f8cd16dc971d8cedfa3
Lucas Soares <lucas_zeroken@hotmail.com>
2019-12-30 16:01:19 +0000

    Added CardService Test

---
+++ src/test/java/com/paulpach/Poker/service/CardServiceTest.java
@@ -0,0 +1,12 @@
+package com.paulpach.Poker.service;
+
+import org.junit.jupiter.api.Test;
+
+public class CardServiceTest {
+
+	@Test
+	public void CardServiceTest() {
+		CardService cardService;
+	}
+	
+}
