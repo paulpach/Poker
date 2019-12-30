cdcb0d72b518cbb2dfacf313bd399e3c61a29226
Lucas Soares <lucas_zeroken@hotmail.com>
2019-12-30 15:18:18 +0000

    Card Model Constructor Added

--- src/main/java/com/paulpach/Poker/model/Card.java
+++ src/main/java/com/paulpach/Poker/model/Card.java
@@ -1,7 +1,14 @@
 package com.paulpach.Poker.model;
 
+import com.paulpach.Poker.model.enums.CardType;
+
 public class Card {
 
+	private Integer id;
+	private CardType cardType;
 	
+	public Card (Integer id, CardType cardType) {
+		
+	}
 	
 }
