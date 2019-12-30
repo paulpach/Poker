f8df202e1489e7794b29cf6d6b362c06a3e675d4
Lucas Soares <lucas_zeroken@hotmail.com>
2019-12-30 22:27:41 +0000

    Added HandTest Test

--- src/main/java/com/paulpach/Poker/model/Hand.java
+++ src/main/java/com/paulpach/Poker/model/Hand.java
@@ -2,4 +2,40 @@ package com.paulpach.Poker.model;
 
 public class Hand {
 
+	private Card card1;
+	private Card card2;
+	private Card card3;
+	private Card card4;
+	private Card card5;
+	
+	public Hand(Card card1, Card card2, Card card3, Card card4, Card card5) {
+		this.card1 = card1;
+		this.card2 = card2;
+		this.card3 = card3;
+		this.card4 = card4;
+		this.card5 = card5;
+	}
+
+	public Card getCard1() {
+		return card1;
+	}
+
+	public Card getCard2() {
+		return card2;
+	}
+
+	public Card getCard3() {
+		return card3;
+	}
+
+	public Card getCard4() {
+		return card4;
+	}
+
+	public Card getCard5() {
+		return card5;
+	}
+
+	
+
 }
--- src/test/java/com/paulpach/Poker/model/HandTest.java
+++ src/test/java/com/paulpach/Poker/model/HandTest.java
@@ -1,12 +1,33 @@
 package com.paulpach.Poker.model;
 
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 import org.junit.jupiter.api.Test;
+
+import com.paulpach.Poker.model.enums.CardType;
 
 public class HandTest {
 
 	@Test
 	public void HandTest() {
 		Hand hand;
+	}
+	
+	@Test
+	public void HandCardsNotNullTest() {
+		Card card1 = new Card(1, CardType.CLUBS);
+		Card card2 = new Card(2, CardType.DIAMONDS);
+		Card card3 = new Card(3, CardType.HEARTS);
+		Card card4 = new Card(4, CardType.SPADES);
+		Card card5 = new Card(5, CardType.CLUBS);
+		
+		Hand hand = new Hand(card1, card2, card3, card4, card5);
+		
+		assertNotEquals(null, hand.getCard1());
+		assertNotEquals(null, hand.getCard2());
+		assertNotEquals(null, hand.getCard3());
+		assertNotEquals(null, hand.getCard4());
+		assertNotEquals(null, hand.getCard5());
 	}
 	
 }
