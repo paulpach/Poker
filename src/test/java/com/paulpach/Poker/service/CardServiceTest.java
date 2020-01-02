3d24378ef801b8db7e7b33cccbc6f1499ce81f82
Paul Pacheco <paulpach@gmail.com>
2020-01-02 14:08:45 -0600

    high card rank test

--- src/test/java/com/paulpach/Poker/service/CardServiceTest.java
+++ src/test/java/com/paulpach/Poker/service/CardServiceTest.java
@@ -11,12 +11,9 @@ import com.paulpach.Poker.model.enums.CardType;
 public class CardServiceTest {
 
 	@Test
-	public void CardServiceTest() {
-		CardService cardService;
-	}
-	
-	@Test
-	public void GetHighCardTest() {
+	public void HighCardTest() {
+        CardService cardService;
+
 		Card card1 = new Card(1, CardType.CLUBS);
 		Card card2 = new Card(12, CardType.DIAMONDS);
 		Card card3 = new Card(7, CardType.HEARTS);
@@ -24,24 +21,10 @@ public class CardServiceTest {
 		Card card5 = new Card(5, CardType.CLUBS);
 		
 		Hand hand = new Hand(card1, card2, card3, card4, card5);
-		
-		Card highestCard = null;
-		
-		if(card1.getValue() > card2.getValue())
-			highestCard = card1;
-		else
-			highestCard = card2;
-		
-		if(card3.getValue() > highestCard.getValue())
-			highestCard = card3;
-		
-		if(card4.getValue() > highestCard.getValue())
-			highestCard = card4;
-		
-		if(card5.getValue() > highestCard.getValue())
-			highestCard = card5;
-		
-		assertEquals(card2, highestCard);
+        
+        Rank rank = cardService.GetRank(hand);
+
+        assertEquals(Rank.HighCard, rank, "This hand's rank is 'high card'");
 	}
 	
 }
